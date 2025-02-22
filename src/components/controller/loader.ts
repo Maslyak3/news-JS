436d7a784b1e38fc6a11e6afc0e41f505e8a3c3c
Roman Masliak <maslyak3@gmail.com>
2025-02-22 14:08:28 +0200

    feat: resolve loader types

---
+++ src/components/controller/loader.ts
@@ -0,0 +1,59 @@
+interface RequestParamModel {
+    endpoint: string;
+    options: Options;
+}
+
+interface Options {
+    [key: string]: string
+}
+
+type CallbackType<T> = (data: T) => void;
+class Loader {
+    baseLink: string;
+    options;
+
+    constructor(baseLink: string, options: Options) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { endpoint, options = {} }: RequestParamModel,
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response) {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options: Options, endpoint: string) {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load<T>(method: string, endpoint: string, callback: CallbackType<T>, options = {}) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data) => callback(data))
+            .catch((err) => console.error(err));
+    }
+}
+
+export default Loader;
